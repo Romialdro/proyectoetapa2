ea8faf5feb27978f292af78d757084d650a48961
Romina <113916711+Romialdro@users.noreply.github.com>
2025-12-04 16:43:37 -0300

    Conexión actualizada al backend en Render

--- src/api/ApiConfig.jsx
+++ src/api/ApiConfig.jsx
@@ -1,5 +1,5 @@
-export const BACKEND_URL = "http://localhost:4000"; 
-//export const API_URL = "https://6908e53d2d902d0651b21158.mockapi.io"; 
+export const BACKEND_URL = "https://backendetapa3.onrender.com";
+
 export const PRODUCTOS_URL = `${BACKEND_URL}/api/productos`;
-export const CONTACTO_URL = `${BACKEND_URL}/api/contacto`; 
+export const CARRITO_URL = `${BACKEND_URL}/api/carrito`;
 
